04830f202aa4e93844193e30eda0e8ae7c5a06d9
Sameer Kohli <skohli1508@gmail.com>
2024-10-11 00:00:15 +0100

    done about me page

--- TheChessBoard-React/src/Components/FrontPage.jsx
+++ TheChessBoard-React/src/Components/FrontPage.jsx
@@ -1,12 +1,24 @@
 import '../css/FrontPage.css';
 import me from '../assets/me.jpeg';
+import flyingtothemoon from '../assets/FlyingtoTheMoon.jpg';
+import ITried from '../assets/ITried.jpg';
 import { useState } from 'react';
 
 export default function FrontPage() {
   let [isVisible, setIsVisible] = useState(false);
+  const [isVisibleFlying, setIsVisibleFlying] = useState(false);
+  const [isVisibleTried, setIsVisibleTried] = useState(false);
 
   function handleClick() {
     onclick = setIsVisible(!isVisible);
+  }
+
+  function handleClick2() {
+    onclick = setIsVisibleFlying(!isVisibleFlying);
+  }
+
+  function handleClick3() {
+    onclick = setIsVisibleTried(!isVisibleTried);
   }
 
   return (
@@ -14,29 +26,63 @@ export default function FrontPage() {
       <div className='mainDiv'>
         <div className='introDiv'>
           <p className='Intro'>
-            B-roll refers to supplemental or alternative footage that is
-            intercut with the main shot in a film or video production. It is
-            often used to enrich the storytelling, providing additional context,
-            visual variety, or coverage.
+            If life were a film, the milestones and highlights would be the
+            A-roll—the polished, front-and-center moments. But here, we are
+            focusing on the B-roll. It is the behind-the-scenes footage, the
+            candid shots, and the unfiltered moments that often go unnoticed but
+            truly tell the full story.
           </p>
         </div>
-        <div className="pictureDiv">
-          <figure onClick={handleClick} className='polaroid'>
-            <img src={me} alt='Polaroid Picture' />
-            <figcaption className='meDescription'>
-              Vibes be vibing <br></br>Aug 24
-            </figcaption>
-            {isVisible ? (
-              <p className='Intro1'>
-                It is in these behind-the-scenes moments that shape who I am.
-                It’s in these everyday experiences, quiet reflections, and
-                unpolished thoughts where any real story unfolds. While the big
-                milestones might be the A-roll, it’s the little details, the
-                candid moments, and the personal reflections—the B-roll—that
-                tell the full story of how I am me...
-              </p>
-            ) : null}
-          </figure>
+        <div className='PictureContainer'>
+          <div className='pictureDiv'>
+            <figure onClick={handleClick} className='polaroid'>
+              <img src={me} alt='Polaroid Picture' />
+              <figcaption className='meDescription'>
+                Vibes be vibing <br></br> Aug 24
+              </figcaption>
+              {isVisible ? (
+                <p className='Intro1'>
+                  It is in these behind-the-scenes moments that shape who I am.
+                  It’s in these everyday experiences, quiet reflections, and
+                  unpolished thoughts where any real story unfolds.
+                </p>
+              ) : null}
+            </figure>
+          </div>
+
+          <div className='pictureDiv secondDiv'>
+            <figure onClick={handleClick2} className='polaroid second'>
+              <img src={flyingtothemoon} alt='Polaroid Picture' />
+              <figcaption className='meDescription'>
+                Flying to the Moon <br></br>  Feb 24
+              </figcaption>
+              {isVisibleFlying ? (
+                <p className='Intro2'>
+                  While the big milestones might be the A-roll, it’s the little
+                  details, the candid moments, and the personal reflections—the
+                  B-roll—that tell the full story of how I am me...
+                </p>
+              ) : null}
+            </figure>
+          </div>
+
+          <div className='pictureDiv thirdDiv'>
+            <figure onClick={handleClick3} className='polaroid third'>
+              <img src={ITried} alt='Polaroid Picture' />
+              <figcaption className='meDescription'>
+                Proffesional Debut <br></br>  Jan 23
+              </figcaption>
+              {isVisibleTried ? (
+                <p className='Intro3'>
+                  Ultimately, these everyday moments come together to tell my
+                  story, with each one reflecting a part of my experience. They
+                  remind me that being open and honest about my life is
+                  important. By sharing these moments—flaws and all—I can show
+                  others the true nature of who I am.
+                </p>
+              ) : null}
+            </figure>
+          </div>
         </div>
       </div>
     </>
--- TheChessBoard-React/src/Components/HowIBecameMe.jsx
+++ TheChessBoard-React/src/Components/HowIBecameMe.jsx
@@ -1,7 +1,71 @@
+import me from '../assets/me.jpeg';
+import '../css/HowIBecameMe.css';
+import data from '../../public/data.json';
+import { useState } from 'react';
+
 export default function HowIBecameMe() {
+  let [isVisible, setIsVisible] = useState(Array(data.length).fill(false));
+
+  // Handle click function to toggle visibility
+  const handleClick = (index) => {
+    console.log(data.length);
+    let newVisibility = [...isVisible];
+    newVisibility[index] = !newVisibility[index];
+    setIsVisible(newVisibility);
+    console.log(newVisibility[index]);
+  };
+
+  let images = {
+    me, // Mapping keys to actual imported images
+  };
+
+  const circleArray = ['22%', '80%', '130%'];
+
   return (
     <>
-      <div> How I became me Check</div>
+      <h1 className='HowIBecameMeTitle'>How I became me</h1>
+
+      <div className='timeline'>
+        {circleArray.map((position, index) => (
+          <div key={index} className='circle' style={{ top: position }}></div>
+        ))}
+      </div>
+
+      <section className='design-section'>
+        <div className='meSection'>
+          {data.map((event, index) => (
+            <div className={`containerBio ${event.alignment}`} key={index}>
+              <figure
+                onClick={() => handleClick(index)}
+                className={`polaroid ${event.alignment} `}
+              >
+                <img
+                  src={images[event.imgSrc]}
+                  alt='Polaroid Picture'
+                  className='HowIBecameMeImg'
+                />
+                <figcaption
+                  className='meDescription'
+                  dangerouslySetInnerHTML={{ __html: event.caption }}
+                />
+              </figure>
+              {isVisible[index] ? (
+                <div className='HowIBecameMeDescription'>
+                  <p className={`HowIBecameMeParagraph ${event.alignment1}`}>
+                    {event.description}
+                  </p>
+                </div>
+              ) : (
+                <div style={{ visibility: 'hidden', height: 'auto' }}>
+                  <p className={`HowIBecameMeParagraph ${event.alignment1}`}>
+                    {event.description}
+                  </p>
+                </div>
+              )}
+            </div>
+          ))}
+        </div>
+      </section>
     </>
   );
 }
